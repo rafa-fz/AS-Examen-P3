2a6caab982a717338b2c1a07c526c0760dc21760
Rafael Fonseca <58662403+rafa-fz@users.noreply.github.com>
2025-03-12 19:59:01 -0500

    Update Factura.java

--- facturacion/facturacion/src/main/java/ec/edu/espe/facturacion/facturacion/model/Factura.java
+++ facturacion/facturacion/src/main/java/ec/edu/espe/facturacion/facturacion/model/Factura.java
@@ -1,5 +1,108 @@
 package ec.edu.espe.facturacion.facturacion.model;
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document
 public class Factura {
+
+    @Id
+    private Integer codFactura;
+    private String tipoIdentificacion;
+    private String identificacion;
+    private String nombre;
+    private LocalDateTime fecha;
+    private BigDecimal subtotal;
+    private BigDecimal iva;
+    private BigDecimal total;
+
+    
+
+
+    public Integer getCodFactura() {
+        return codFactura;
+    }
+    public void setCodFactura(Integer codFactura) {
+        this.codFactura = codFactura;
+    }
+    public String getTipoIdentificacion() {
+        return tipoIdentificacion;
+    }
+    public void setTipoIdentificacion(String tipoIdentificacion) {
+        this.tipoIdentificacion = tipoIdentificacion;
+    }
+    public String getIdentificacion() {
+        return identificacion;
+    }
+    public void setIdentificacion(String identificacion) {
+        this.identificacion = identificacion;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public LocalDateTime getFecha() {
+        return fecha;
+    }
+    public void setFecha(LocalDateTime fecha) {
+        this.fecha = fecha;
+    }
+    public BigDecimal getSubtotal() {
+        return subtotal;
+    }
+    public void setSubtotal(BigDecimal subtotal) {
+        this.subtotal = subtotal;
+    }
+    public BigDecimal getIva() {
+        return iva;
+    }
+    public void setIva(BigDecimal iva) {
+        this.iva = iva;
+    }
+    public BigDecimal getTotal() {
+        return total;
+    }
+    public void setTotal(BigDecimal total) {
+        this.total = total;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((codFactura == null) ? 0 : codFactura.hashCode());
+        return result;
+    }
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Factura other = (Factura) obj;
+        if (codFactura == null) {
+            if (other.codFactura != null)
+                return false;
+        } else if (!codFactura.equals(other.codFactura))
+            return false;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Factura [codFactura=" + codFactura + ", tipoIdentificacion=" + tipoIdentificacion + ", identificacion="
+                + identificacion + ", nombre=" + nombre + ", fecha=" + fecha + ", subtotal=" + subtotal + ", iva=" + iva
+                + ", total=" + total + "]";
+    }
+
+    
+
 
 }
